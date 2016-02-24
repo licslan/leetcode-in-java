e43f1fe7052adf0279e9f664b16694e2b0972b9b
Shijun Qin <272700767@qq.com>
2016-02-24 23:34:19 +0800

    Longest Substring Without Repeating Characters

---
+++ 3-longest-substring-without-repeating-characters/Solution.java
@@ -0,0 +1,22 @@
+public class Solution {
+    public int lengthOfLongestSubstring(String s) {
+		if (s == null)
+			return 0;
+		char[] arr = s.toCharArray();
+		int max = 0;
+
+		HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+
+		for (int i = 0; i < arr.length; i++) {
+			if (!map.containsKey(arr[i])) {
+				map.put(arr[i], i);
+			} else {
+				max = Math.max(max, map.size());
+				i = map.get(arr[i]);
+				map.clear();
+			}
+		}
+
+		return Math.max(max, map.size());
+	}
+}
