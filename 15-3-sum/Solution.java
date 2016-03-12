23aab1085d05a174ba1c3238c665b6e039806c9c
Shijun Qin <272700767@qq.com>
2016-03-12 15:04:36 +0800

    add

--- 15-3-sum/Solution.java
+++ 15-3-sum/Solution.java
@@ -1,23 +1,24 @@
 public class Solution {
 	public List<List<Integer>> threeSum(int[] nums) {
-		Arrays.sort(nums);//先对数组进行排序
+		Arrays.sort(nums);// 先对数组进行排序
 		List<List<Integer>> list = new ArrayList<List<Integer>>();
 		for (int i = 0; i < nums.length - 2; i++) {
 			if (i > 0 && nums[i] == nums[i - 1])// 防止重复元素
 				continue;
-			for (int j = i + 1, k = nums.length - 1; j < k;) {
-				if (nums[i] + nums[j] + nums[k] == 0) {
-					list.add(Arrays.asList(nums[i], nums[j], nums[k]));
-					j++;
-					k--;
-					while (j < k && nums[j] == nums[j - 1])
-						j++;// 防止重复元素
-					while (j < k && nums[k] == nums[k + 1])
-						k--;// 防止重复元素
-				} else if (nums[i] + nums[j] + nums[k] > 0) {
-					k--;
+			int start = i + 1, end = nums.length - 1;
+			while (start < end) {
+				if (nums[i] + nums[start] + nums[end] == 0) {
+					list.add(Arrays.asList(nums[i], nums[start], nums[end]));
+					start++;
+					end--;
+					while (start < end && nums[start] == nums[start - 1])
+						start++;// 防止重复元素
+					while (start < end && nums[end] == nums[end + 1])
+						end--;// 防止重复元素
+				} else if (nums[i] + nums[start] + nums[end] > 0) {
+					end--;
 				} else {
-					j++;
+					start++;
 				}
 			}
 		}
