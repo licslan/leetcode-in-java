51916a98667ab6de1c2641f963c07fca3cb8238f
Shijun Qin <272700767@qq.com>
2016-03-29 17:57:29 +0800

    Search Insert Position

---
+++ 35-search-insert-position/Solution.java
@@ -0,0 +1,9 @@
+public class Solution {
+	public int searchInsert(int[] nums, int target) {
+	    if (target < nums[0]) return 0;
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] >= target) return i;
+		}
+		return nums.length;
+	}
+}
