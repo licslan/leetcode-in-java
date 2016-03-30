ab25845dbfab655219cdddb1c078fab3006e0a6c
Shijun Qin <272700767@qq.com>
2016-03-30 13:03:15 +0800

    Combination Sum

---
+++ 39-combination-sum/Solution.java
@@ -0,0 +1,26 @@
+public class Solution {
+	public List<List<Integer>> combinationSum(int[] candidates, int target) {
+		Arrays.sort(candidates);
+		return combinationSum(candidates, target, 0);
+	}
+
+	public List<List<Integer>> combinationSum(int[] candidates, int target, int start) {
+
+		List<List<Integer>> res = new LinkedList<List<Integer>>();
+        List<Integer> lst = new ArrayList<>();
+        
+		for (int i = start; i < candidates.length; i++) {
+			if (candidates[i] < target) {
+				for (List<Integer> ar : combinationSum(candidates, target - candidates[i], i)) {
+					ar.add(0, candidates[i]);
+					res.add(ar);
+				}
+			} else if (candidates[i] == target) {
+				lst.add(candidates[i]);
+				res.add(lst);
+			} else
+				break;
+		}
+		return res;
+	}
+}
