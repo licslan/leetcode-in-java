ade279dae9ae6d8d4861a70bc90bed42496d0d18
Shijun Qin <272700767@qq.com>
2016-04-08 16:31:28 +0800

    Permutation Sequence

---
+++ 60-permutation-sequence/Solution.java
@@ -0,0 +1,17 @@
+public class Solution {
+	public String getPermutation(int n, int k) {
+		LinkedList<Integer> list = new LinkedList<>();
+		for (int i = 1; i <= n; i++)
+			list.add(i);
+		int fact = 1;
+		for (int i = 2; i <= n; i++)
+			fact *= i; // factorial
+		StringBuilder strBuilder = new StringBuilder();
+		for (k--; n > 0; n--) {
+			fact /= n;
+			strBuilder.append(list.remove(k / fact));
+			k %= fact;
+		}
+		return strBuilder.toString();
+	}
+}
