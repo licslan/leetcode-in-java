0c567c17eebd2739c1f4bfb3b9ebe9f6ebfc7ace
Shijun Qin <272700767@qq.com>
2016-05-06 20:21:22 +0800

    fix

--- 97-interleaving-string/Solution.java
+++ 97-interleaving-string/Solution.java
@@ -3,11 +3,11 @@
 public class Solution {
     public boolean isInterleave(String s1, String s2, String s3) {
         char[] c1 = s1.toCharArray(), c2 = s2.toCharArray(), c3 = s3.toCharArray();
-        if (s1.length() + s2.length() != s3.length())
+        if (c1.length + c2.length != c3.length)
             return false;
-        boolean[][] table = new boolean[s1.length() + 1][s2.length() + 1];
-        for (int i = 0; i < s1.length() + 1; i++) {
-            for (int j = 0; j < s2.length() + 1; j++) {
+        boolean[][] table = new boolean[c1.length + 1][c2.length + 1];
+        for (int i = 0; i < c1.length + 1; i++) {
+            for (int j = 0; j < c2.length + 1; j++) {
                 if (i == 0 && j == 0) {
                     table[i][j] = true;
                 } else if (i == 0) {
@@ -20,6 +20,6 @@ public class Solution {
                 }
             }
         }
-        return table[s1.length()][s2.length()];
+        return table[c1.length][c2.length];
     }
 }
