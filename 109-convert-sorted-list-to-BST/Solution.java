db2ce39d7ef77445dd9c43d0a37a297ec41fc336
Shijun Qin <272700767@qq.com>
2016-05-10 17:18:46 +0800

    Convert Sorted List to BST

---
+++ 109-convert-sorted-list-to-BST/Solution.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public TreeNode sortedListToBST(ListNode head) {
+        return buildBST(head, null);
+    }
+
+    public TreeNode buildBST(ListNode head, ListNode tail) {
+        if (head == tail)
+            return null;
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != tail && fast.next != tail) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        TreeNode node = new TreeNode(slow.val);
+        node.left = buildBST(head, slow);
+        node.right = buildBST(slow.next, tail);
+        return node;
+    }
+}
