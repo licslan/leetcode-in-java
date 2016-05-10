baafe7d01071fc6be4805fb23d9677c33258c5f0
Shijun Qin <272700767@qq.com>
2016-05-10 17:22:47 +0800

    fix

---
+++ 108-convert-sorted-array-to-BST/Solution.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return buildBST(nums, 0, nums.length - 1);
+    }
+
+    // 递归方法
+    public TreeNode buildBST(int[] nums, int start, int end) {
+        if (start > end)
+            return null;
+        int mid = (start + end) / 2;
+        TreeNode root = new TreeNode(nums[mid]);
+        root.left = buildBST(nums, start, mid - 1);
+        root.right = buildBST(nums, mid + 1, end);
+        return root;
+    }
+}
