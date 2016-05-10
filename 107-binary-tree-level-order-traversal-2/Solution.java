e24b7b2505d5ecc58533d532b813ab001e99c601
Shijun Qin <272700767@qq.com>
2016-05-10 17:27:09 +0800

    fix

--- 107-binary-tree-level-order-traversal-2/Solution.java
+++ 107-binary-tree-level-order-traversal-2/Solution.java
@@ -8,18 +8,21 @@
  * }
  */
 public class Solution {
-    public TreeNode sortedArrayToBST(int[] nums) {
-        return buildTree(nums, 0, nums.length - 1);
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        buildList(root, 0, res);
+        Collections.reverse(res);
+        return res;
     }
 
-    // 递归方法
-    public TreeNode buildTree(int[] nums, int start, int end) {
-        if (start > end)
-            return null;
-        int mid = (start + end) / 2;
-        TreeNode root = new TreeNode(nums[mid]);
-        root.left = buildTree(nums, start, mid - 1);
-        root.right = buildTree(nums, mid + 1, end);
-        return root;
+    // 先序遍历
+    public void buildList(TreeNode root, int depth, List<List<Integer>> res) {
+        if (root == null)
+            return;
+        if (res.size() == depth)
+            res.add(new ArrayList<Integer>());
+        res.get(depth).add(root.val);
+        buildList(root.left, depth + 1, res);
+        buildList(root.right, depth + 1, res);
     }
 }
