82b1a239e8c7024c6448c176067fdbf1e6404299
Shijun Qin <272700767@qq.com>
2016-05-16 20:25:54 +0800

    Single Number

---
+++ 136-single-number/Solution.java
@@ -0,0 +1,9 @@
+public class Solution {
+    // XOR，异或具有交换性，相同的两个数异或结果为0，0和任意一个数异或结果为那个数
+    public int singleNumber(int[] nums) {
+        int res = 0;
+        for (int num : nums)
+            res ^= num;
+        return res;
+    }
+}
