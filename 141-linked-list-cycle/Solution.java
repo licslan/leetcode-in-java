7af4c85ef98ea77d01aa251a4d2fe9a25826ec09
Shijun Qin <272700767@qq.com>
2016-05-20 09:39:50 +0800

    Linked List Cycle

---
+++ 141-linked-list-cycle/Solution.java
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    // 要求不适用额外的空间，故不能用HashMap
+    public boolean hasCycle(ListNode head) {
+        ListNode fast = head, slow = head;
+        while (slow != null && fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) return true;
+        }
+        return false;
+    }
+}
