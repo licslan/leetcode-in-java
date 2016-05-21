bfb2efac4a800eec6ac710b5687111660ec7d83b
Shijun Qin <272700767@qq.com>
2016-05-21 12:41:07 +0800

    fix

--- 141-linked-list-cycle/Solution.java
+++ 141-linked-list-cycle/Solution.java
@@ -13,7 +13,7 @@ public class Solution {
     // 要求不适用额外的空间，故不能用HashMap
     public boolean hasCycle(ListNode head) {
         ListNode fast = head, slow = head;
-        while (slow != null && fast != null && fast.next != null) {
+        while (fast != null && fast.next != null) {// 这里并不用slow!=null;
             fast = fast.next.next;
             slow = slow.next;
             if (fast == slow) return true;
