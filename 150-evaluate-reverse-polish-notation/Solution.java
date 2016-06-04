2bb6a07f810fa865303b69b7d4846bde662af00b
Shijun Qin <272700767@qq.com>
2016-06-04 22:02:13 +0800

    Evaluate Reverse Polish Notation

---
+++ 150-evaluate-reverse-polish-notation/Solution.java
@@ -0,0 +1,24 @@
+public class Solution {
+    public int evalRPN(String[] tokens) {
+        int a, b;
+        Stack<Integer> stack = new Stack<>();
+        for (String s : tokens) {
+            if (s.equals("+")) {
+                stack.add(stack.pop() + stack.pop());
+            } else if (s.equals("-")) {
+                b = stack.pop();
+                a = stack.pop();
+                stack.add(a - b);
+            } else if (s.equals("*")) {
+                stack.add(stack.pop() * stack.pop());
+            } else if (s.equals("/")) {
+                b = stack.pop();
+                a = stack.pop();
+                stack.add(a / b);
+            } else {
+                stack.add(Integer.parseInt(s));
+            }
+        }
+        return stack.pop();
+    }
+}
