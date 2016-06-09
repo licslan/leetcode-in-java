a70e469a33bc2f79f13116edd7293584b73ce86e
Shijun Qin <272700767@qq.com>
2016-06-09 22:32:27 +0800

    Repeated DNA Sequences

---
+++ 187-repeated-DNA-sequences/Solution.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public class Solution {
+        public List<String> findRepeatedDnaSequences(String s) {
+            Set<String> seen = new HashSet<>(),repeated = new HashSet<>();
+            for(int i=0;i+9<s.length();i++){
+                String ten = s.substring(i,i+10);
+                if(!seen.add(ten))// true if this set did not already contain the specified element
+                    repeated.add(ten);
+            }
+            return new ArrayList<>(repeated);
+        }
+    }
+}
