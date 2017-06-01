099cfa1be0b785885995552543bc1f3ff292194c
lioncruise <272700767@qq.com>
2017-06-01 19:54:26 +0800

    566. Reshape the Matrix

---
+++ 566-reshape-the-matrix/Solution.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public int[][] matrixReshape(int[][] nums, int r, int c) {
+        // n 这个矩阵有多少行，二维数组的第一个下标
+        // n 这个矩阵有多少列（一行有多少个元素），二维数组的第二个下标
+        int n = nums.length, m = nums[0].length;
+
+        // 很显然，不合法的条件就是变形后矩阵元素的个数和原矩阵不一样
+        if (r * c != m * n)
+            return nums;
+
+        // 有点像把二维数组转化成一维数组
+        int[][] res = new int[r][c];
+        for (int i = 0; i < r * c; i++)
+            res[i / c][i % c] = nums[i / m][i % m];
+        return res;
+    }
+}
