4d89ba4c05385f86229dc27d9c722e6a34502044
lioncruise <272700767@qq.com>
2017-07-30 00:06:13 +0800

    fix two sum

--- 001-two-sum/Solution.java
+++ 001-two-sum/Solution.java
@@ -1,15 +1,16 @@
 public class Solution {
     public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
-        for (int i = 0; i < nums.length; i++) {
+        Map<Integer, Integer> map = new HashMap<>();
+        // 将值作为key，索引作为value
+        for (int i = 0; i < nums.length; ++i)
             map.put(nums[i], i);
+
+        for (int i = 0; i < nums.length; ++i) {
+            // 题目中有个条件：you may not use the same element twice
+            if (map.containsKey(target - nums[i]) && (map.get(target - nums[i]) != i))
+                return new int[]{i, map.get(target - nums[i])};
         }
 
-        for (int i = 0; i < nums.length; i++) {
-            if (map.containsKey(target - nums[i]) && (map.get(target - nums[i]) != i)) {
-                return new int[] { i, map.get(target - nums[i]) };
-            }
-        }
-        return new int[] { -1, -1 };
+        return new int[]{-1, -1};
     }
 }
