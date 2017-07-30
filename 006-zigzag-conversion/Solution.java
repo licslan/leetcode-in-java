9f3dff81b9f14e6009ea042998a6610b297b6a98
lioncruise <272700767@qq.com>
2017-07-30 23:24:13 +0800

    fix zigzag conversion

--- 006-zigzag-conversion/Solution.java
+++ 006-zigzag-conversion/Solution.java
@@ -1,21 +1,22 @@
 public class Solution {
     public String convert(String s, int numRows) {
-        if(numRows==1) return s;
-        int x = 2 * (numRows-1); // distance between pipes |/|/|...
+        if (numRows == 1)
+            return s;
+
+        int x = 2 * numRows - 2;
         int len = s.length();
         char[] c = new char[len];
-        int k =0;
-        for(int i=0; i < numRows; i++)
-        {
-            for(int j=i;j<len;j=j+x)
-            {
+        int k = 0;
+
+        for (int i = 0; i < numRows; ++i) {
+            for (int j = i; j < len; j += x) {
                 c[k++] = s.charAt(j);
-                if(i>0 && i<numRows-1 && j+x-2*i < len)
-                {
-                       c[k++] = s.charAt(j+x-2*i); // extra character between pipes
-                }
+                // 注意这里的下界是numRows-2，不是numRows-1
+                if (i > 0 && i < numRows - 1 && j + x - 2 * i < len)
+                    c[k++] = s.charAt(j + x - 2 * i);
             }
         }
+
         return new String(c);
     }
 }
