7b6fc41c6f1678973274a2ac37c4d8392cf374dc
lioncruise <272700767@qq.com>
2017-07-31 10:05:46 +0800

    fix reverse integer

--- 007-reverse-integer/Solution.java
+++ 007-reverse-integer/Solution.java
@@ -1,10 +1,16 @@
 public class Solution {
     public int reverse(int x) {
         long res = 0;
-        for (; x != 0; x /= 10) {
-            res = res * 10 + x % 10;
-        }
-        // int x倒转过来可能会变成long，这时要强转一下
-        return res > Integer.MAX_VALUE || res < Integer.MIN_VALUE ? 0 : (int) res;
+
+        // 在Java中，-9999%10=-9，所以负数也可以这样除
+        for (; x != 0; x /= 10)
+            res = 10 * res + x % 10;
+
+        // 题目要求中说，当溢出int范围时，返回0
+        if (res > Integer.MAX_VALUE || res < Integer.MIN_VALUE)
+            return 0;
+
+        // 这里返回类型是int，需要将long类型强转成int
+        return (int) res;
     }
 }
