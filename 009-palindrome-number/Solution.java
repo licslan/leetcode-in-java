df5a6423fa3bca32d985efdc0cb8fe058011e32e
lioncruise <272700767@qq.com>
2017-07-31 16:42:03 +0800

    add palindrome number

--- 009-palindrome-number/Solution.java
+++ 009-palindrome-number/Solution.java
@@ -10,4 +10,19 @@ public class Solution {
         }
         return y < 10 * x ? y == x : y / 10 == x;
     }
+
+
+    // 一种更好理解的方法
+    public boolean isPalindrome1(int x) {
+        // 不除掉末尾带0的，{回文数字}0...0 这种数字会被判为回文的
+        if (x < 0 || (x != 0 && x % 10 == 0))
+            return false;
+
+        int y = 0;  // y表示从个位开始数，x表示从高位开始数
+        while (x > y) {
+            y = 10 * y + x % 10;
+            x /= 10;
+        }
+        return (x == y) || (x == y / 10);
+    }
 }
