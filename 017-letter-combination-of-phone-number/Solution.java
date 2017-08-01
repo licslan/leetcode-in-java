9847efd379829a8c53e1c002f7ac9828f94c7f05
lioncruise <272700767@qq.com>
2017-08-01 23:33:40 +0800

    fix letter combinations

--- 017-letter-combination-of-phone-number/Solution.java
+++ 017-letter-combination-of-phone-number/Solution.java
@@ -1,13 +1,17 @@
 public class Solution {
-    String[][] refer = { {}, {}, { "a", "c", "b" }, { "d", "e", "f" }, { "g", "h", "i" }, { "j", "k", "l" },
-            { "m", "n", "o" }, { "p", "q", "r", "s" }, { "t", "u", "v" }, { "w", "x", "y", "z" } };
+    String[][] refer = {{}, {}, {"a", "b", "c"}, {"d", "e", "f"},
+            {"g", "h", "i"}, {"j", "k", "l"}, {"m", "n", "o"},
+            {"p", "q", "r", "s"}, {"t", "u", "v"}, {"w", "x", "y", "z"}};
 
     public List<String> letterCombinations(String digits) {
-        List<String> list = new ArrayList<String>();
-        if (!digits.equals("")) {
-            helper(list, digits, "");
-        }
-        return list;
+        List<String> res = new ArrayList<>();
+
+        // 当digits为""时，res应该为[]，而不是[""]
+        if (digits == null || digits.length() == 0)
+            return res;
+
+        helper(res, digits, "");
+        return res;
     }
 
     private void helper(List<String> list, String digits, String s) {
@@ -15,9 +19,10 @@ public class Solution {
             list.add(s);
             return;
         }
+
         int idx = Integer.parseInt(digits.substring(0, 1));
-        for (String k : refer[idx]) {
-            helper(list, digits.substring(1, digits.length()), s + k);
+        for (String e : refer[idx]) {
+            helper(list, digits.substring(1, digits.length()), s + e);
         }
     }
 }
