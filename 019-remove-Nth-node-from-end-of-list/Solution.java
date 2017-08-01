45c96ecc13cfe7c68ad8d819acc1cdfc3436b692
lioncruise <272700767@qq.com>
2017-08-02 00:05:18 +0800

    fix remove Nth node from end of the list

--- 019-remove-Nth-node-from-end-of-list/Solution.java
+++ 019-remove-Nth-node-from-end-of-list/Solution.java
@@ -8,19 +8,26 @@
  */
 public class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        ListNode slow = head;
         ListNode fast = head;
-        for (int i = 0; i < n; i++) {
+        ListNode slow = head;
+
+        // fast节点比slow节点快n步，fast节点为倒数第一个时，slow节点为倒数第n+1个节点
+        for (int i = 0; i < n; ++i)
             fast = fast.next;
-        }
-        if (fast == null) {
-            return slow.next;
-        }
+
+        // 如果fast跑到了null，说明第第一个节点就是倒数第n个节点。直接返回head.next，否则会空指针异常。
+        if (fast == null)
+            return head.next;
+
+        // 要删除倒数第n个节点，关键是要找到它的前面一个节点，即倒数第n+1个节点
+        // 将fast移动到最后一个节点，fast.next为null
         while (fast.next != null) {
-            slow = slow.next;
             fast = fast.next;
+            slow = slow.next;
         }
+
         slow.next = slow.next.next;
+
         return head;
     }
 }
