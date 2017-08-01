475f8fd054523f50e8ebd96caf39dbff7c1ddb46
lioncruise <272700767@qq.com>
2017-08-01 13:52:45 +0800

    fix longest common prefix

--- 014-longest-common-prefix/Solution.java
+++ 014-longest-common-prefix/Solution.java
@@ -1,11 +1,18 @@
 public class Solution {
     public String longestCommonPrefix(String[] strs) {
-        int n=strs.length;
-        if(n==0) return "";
-        StringBuilder st=new StringBuilder(strs[0]);
-        for(int i=1;i<n;i++){
-            while(!strs[i].startsWith(st.toString())) st.deleteCharAt(st.length()-1);
+        if (strs == null || strs.length == 0)
+            return "";
+
+        String pre = strs[0];
+
+        // 从第二个String开始比
+        for (int i = 1; i < strs.length; ++i) {
+            // String的indexOf这个方法，返回指定字符串第一次出现的位置；如果没出现，返回-1
+            while (strs[i].indexOf(pre) != 0)
+                // substring(m,n)方法截取[m,n)个字符
+                pre = pre.substring(0, pre.length() - 1);
+
         }
-        return st.toString();
+        return pre;
     }
 }
