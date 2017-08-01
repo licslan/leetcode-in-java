1e3a1387bbf4d83d2c7d2138f9ecad2506aad295
lioncruise <272700767@qq.com>
2017-08-01 16:25:38 +0800

    fix 3sum-closest

--- 016-3-sum-closest/Solution.java
+++ 016-3-sum-closest/Solution.java
@@ -1,21 +1,24 @@
 public class Solution {
-        public int threeSumClosest(int[] num, int target) {
-        int result = num[0] + num[1] + num[num.length - 1];
-        Arrays.sort(num);//先将数组排序
-        for (int i = 0; i < num.length - 2; i++) {
-            int start = i + 1, end = num.length - 1;
-            while (start < end) {
-                int sum = num[i] + num[start] + num[end];
-                if (sum > target) {
-                    end--;
-                } else {
-                    start++;
-                }
-                if (Math.abs(sum - target) < Math.abs(result - target)) {
-                    result = sum;
-                }
+    // 这题假定nums最少有3个元素
+    public int threeSumClosest(int[] nums, int target) {
+        // 首先一定要对nums排序，否则否则后面的操作都是没有意义的
+        Arrays.sort(nums);
+
+        int res = nums[0] + nums[1] + nums[nums.length - 1];
+        for (int i = 0; i < nums.length - 2; ++i) {
+            int low = i + 1, high = nums.length - 1;
+            while (low < high) {
+                int sum = nums[i] + nums[low] + nums[high];
+                if (sum == target)
+                    return target;
+                else if (sum > target)
+                    --high;
+                else
+                    ++low;
+                if (Math.abs(sum - target) < Math.abs(res - target))
+                    res = sum;
             }
         }
-        return result;
+        return res;
     }
 }
