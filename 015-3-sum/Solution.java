575c6ae2eb82bd882041ead213c2171c4b0f7673
lioncruise <272700767@qq.com>
2017-08-01 15:23:55 +0800

    fix 3sum

--- 015-3-sum/Solution.java
+++ 015-3-sum/Solution.java
@@ -1,27 +1,32 @@
 public class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        Arrays.sort(nums);// 先对数组进行排序
-        List<List<Integer>> list = new ArrayList<List<Integer>>();
-        for (int i = 0; i < nums.length - 2; i++) {
-            if (i > 0 && nums[i] == nums[i - 1])// 防止重复元素
+        Arrays.sort(nums);
+
+        List<List<Integer>> res = new ArrayList<>();
+
+        for (int i = 0; i < nums.length - 2; ++i) {
+            // 一定要有，去除掉重复的元组
+            if (i > 0 && nums[i] == nums[i - 1])
                 continue;
-            int start = i + 1, end = nums.length - 1;
-            while (start < end) {
-                if (nums[i] + nums[start] + nums[end] == 0) {
-                    list.add(Arrays.asList(nums[i], nums[start], nums[end]));
-                    start++;
-                    end--;
-                    while (start < end && nums[start] == nums[start - 1])
-                        start++;// 防止重复元素
-                    while (start < end && nums[end] == nums[end + 1])
-                        end--;// 防止重复元素
-                } else if (nums[i] + nums[start] + nums[end] > 0) {
-                    end--;
-                } else {
-                    start++;
-                }
+
+            int low = i + 1, high = nums.length - 1;
+            while (low < high) {
+                if (nums[i] + nums[low] + nums[high] == 0) {
+                    res.add(Arrays.asList(nums[i], nums[low], nums[high]));
+
+                    ++low;
+                    --high;
+                    // 去除掉重复的元组
+                    while (low < high && nums[low] == nums[low - 1])
+                        ++low;
+                    while (low < high && nums[high] == nums[high + 1])
+                        --high;
+                } else if (nums[i] + nums[low] + nums[high] > 0)
+                    --high;
+                else
+                    ++low;
             }
         }
-        return list;
+        return res;
     }
 }
