cdfd88188ef0b320e4a29ec987aeb2818583c290
lioncruise <272700767@qq.com>
2017-08-02 13:24:46 +0800

    fix merge two sorted lists

--- 021-merge-two-sorted-lists/Solution.java
+++ 021-merge-two-sorted-lists/Solution.java
@@ -7,11 +7,15 @@
  * }
  */
 public class Solution {
+    // 递归解法
+    // 这个解法不是一个尾递归，在两个链表过长的时候会发生 栈溢出
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         if (l1 == null)
             return l2;
         if (l2 == null)
             return l1;
+
+        // 如果能到这一步，l1和l2都不为空
         if (l1.val < l2.val) {
             l1.next = mergeTwoLists(l1.next, l2);
             return l1;
