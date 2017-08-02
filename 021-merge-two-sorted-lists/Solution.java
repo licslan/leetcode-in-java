edba2e92ffa3b6da52e235933889c383770535f5
lioncruise <272700767@qq.com>
2017-08-02 13:58:27 +0800

    add merge two sorted lists

--- 021-merge-two-sorted-lists/Solution.java
+++ 021-merge-two-sorted-lists/Solution.java
@@ -24,4 +24,27 @@ public class Solution {
             return l2;
         }
     }
+
+
+    // 迭代解法，使用两个辅助节点，一个pre记录头部，一个curr跟踪新的list
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode(0);
+        ListNode curr = head;
+
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                curr.next = l1;
+                l1 = l1.next;
+
+            } else {
+                curr.next = l2;
+                l2 = l2.next;
+            }
+            curr = curr.next;
+        }
+
+        curr.next = l1 == null ? l2 : l1;
+
+        return head.next;
+    }
 }
