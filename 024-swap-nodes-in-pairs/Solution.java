7f1c2d02f72b6efc9b8a5f67e2dd805b9f01c071
lioncruise <272700767@qq.com>
2017-08-05 16:34:45 +0800

    fix swap nodes in pairs

--- 024-swap-nodes-in-pairs/Solution.java
+++ 024-swap-nodes-in-pairs/Solution.java
@@ -1,25 +1,35 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) { val = x; }
- * }
- */
 public class Solution {
+    // 递归写法
     public ListNode swapPairs(ListNode head) {
-        ListNode start = new ListNode(0); // make head no longer a special case
-        start.next = head;
+        // 当没有节点或者只有一节节点的时候，不用交换节点，直接返回head
+        // 递归的运行过程是先将链表的尾部计算好，再一点点往头部添加，知道添加完为止
+        if (head == null || head.next == null)
+            return head;
 
-        for (ListNode cur = start; cur.next != null && cur.next.next != null; cur = cur.next.next) {
-            cur.next = swap(cur.next, cur.next.next);
-        }
-        return start.next;
+        ListNode temp = head.next;
+        head.next = swapPairs(head.next.next);
+        temp.next = head;
+
+        return temp;
     }
 
-    private ListNode swap(ListNode next1, ListNode next2) {
-        next1.next = next2.next;
-        next2.next = next1;
-        return next2;
+
+    // 迭代写法
+    public ListNode swapPairs1(ListNode head) {
+        // 前置辅助节点，以便后面好返回新的链表头
+        ListNode pre = new ListNode(0);
+        pre.next = head;
+
+        // 这里跟踪指针从pre节点开始，不是从head节点开始
+        for (ListNode curr = pre; curr.next != null && curr.next.next != null; curr = curr.next.next)
+            curr.next = swap(curr.next, curr.next.next);
+
+        return pre.next;
+    }
+
+    private ListNode swap(ListNode node1, ListNode node2) {
+        node1.next = node2.next;
+        node2.next = node1;
+        return node2;
     }
 }
